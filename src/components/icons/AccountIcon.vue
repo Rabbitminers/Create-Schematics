5d6b2fd0b857a27c5b3f655783cbc40c9ec28614
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-15 22:15:31 +0100

    Added Acount icon

---
+++ src/components/icons/AccountIcon.vue
@@ -0,0 +1,13 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 16 16" shape-rendering="crispEdges">
+        <metadata>Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj</metadata>
+        <path stroke="#ffffff" d="M5 0h1M2 3h1M0 9h1M6 10h1M9 10h2M14 10h1M4 11h1M11 11h1M3 13h1M11 14h1M9 15h1" />
+        <path stroke="#ffffff" d="M6 0h2M10 1h1M2 2h2M13 2h1M1 3h1M7 4h2M1 5h1M15 5h1M0 6h1M15 6h1M10 7h1M5 8h1M6 9h4M0 10h1M5 10h1M15 10h1M14 11h1M2 12h2M14 12h1M3 14h2M10 14h1M5 15h1M7 15h2" />
+        <path stroke="#ffffff" d="M8 0h1M4 1h2M11 1h2M14 3h1M1 4h1M9 4h1M0 5h1M5 5h1M9 5h1M5 6h1M15 7h1M1 12h1M12 12h1M2 13h1M13 13h1M12 14h1M6 15h1" />
+        <path stroke="#ffffff" d="M9 0h1M3 1h1M12 2h1M13 3h1M6 4h1M10 5h1M14 5h1M10 6h1M0 8h1M6 8h1M9 8h2M15 8h1M1 10h1M4 10h1M11 10h1M3 11h1M12 13h1M5 14h1" />
+        <path stroke="#ffffff" d="M10 0h1M6 5h1M0 7h1M15 9h1" />
+        <path stroke="#ffffff" d="M14 4h1M5 7h1" />
+        <path stroke="#ffffff" d="M1 11h1M12 11h1M10 15h1" />
+        <path stroke="#ffffff" d="M13 12h1" />
+    </svg>
+</template>
