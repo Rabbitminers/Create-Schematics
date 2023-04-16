6ef5b5da798f036a7fcb2e0643e23866ff5222a1
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-16 18:25:01 +0100

    Added Schematic Filter

--- backend/src/models/filters.rs
+++ backend/src/models/filters.rs
@@ -1,3 +1,5 @@
+use chrono::NaiveDateTime;
+
 #[derive(Deserialize)]
 pub struct PersonFilter {
     pub name: Option<String>,
@@ -6,6 +8,18 @@ pub struct PersonFilter {
     pub address: Option<String>,
     pub phone: Option<String>,
     pub email: Option<String>,
+    pub sort_by: Option<String>,
+    pub sort_direction: Option<String>,
+    pub page_num: Option<i64>,
+    pub page_size: Option<i64>,
+}
+
+#[derive(Deserialize)]
+pub struct SchematicFilter {
+    pub title: Option<String>,
+    pub author: Option<i32>,
+    pub tags: Option<String>,
+    pub date: Option<NaiveDateTime>,
     pub sort_by: Option<String>,
     pub sort_direction: Option<String>,
     pub page_num: Option<i64>,
