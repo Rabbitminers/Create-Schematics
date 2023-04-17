aee6af40e1b578cb26ad83ddebdcf81c9c6c3afb
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-17 18:27:24 +0100

    Added update and delete schematic services

--- backend/src/api/schematic_controller.rs
+++ backend/src/api/schematic_controller.rs
@@ -40,3 +40,44 @@ pub async fn search(
         Err(err) => Ok(err.response()),
     }
 }
+
+// PUT api/schematic/{id}/update
+pub async fn update(
+    id: web::Path<String>,
+    updated_schematic: web::Json<SchematicDTO>,
+    req: HttpRequest,
+    pool: web::Data<Pool>,
+) -> Result<HttpResponse> {
+    if let Some(authen_header) = req.headers().get(constants::AUTHORIZATION) {
+        schematic_service::update(id.into_inner(), authen_header, updated_schematic.0, &pool);
+        Ok(HttpResponse::Ok().json(ResponseBody::new(
+            constants::MESSAGE_OK,
+            constants::EMPTY,
+        )))
+    } else {
+        Ok(HttpResponse::BadRequest().json(ResponseBody::new(
+            constants::MESSAGE_TOKEN_MISSING,
+            constants::EMPTY,
+        )))
+    }
+}
+
+// PUT api/schematic/{id}/delete
+pub async fn delete(
+    id: web::Path<String>,
+    req: HttpRequest,
+    pool: web::Data<Pool>,
+) -> Result<HttpResponse> {
+    if let Some(authen_header) = req.headers().get(constants::AUTHORIZATION) {
+        schematic_service::delete(id.into_inner(), authen_header, &pool);
+        Ok(HttpResponse::Ok().json(ResponseBody::new(
+            constants::MESSAGE_OK,
+            constants::EMPTY,
+        )))
+    } else {
+        Ok(HttpResponse::BadRequest().json(ResponseBody::new(
+            constants::MESSAGE_TOKEN_MISSING,
+            constants::EMPTY,
+        )))
+    }
+}
