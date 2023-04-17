2aca43c0d25e9cfeeeba88514d8c8c9503df5191
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-17 21:46:28 +0100

    Implemented Comment Filtering

--- backend/src/models/filters.rs
+++ backend/src/models/filters.rs
@@ -25,3 +25,14 @@ pub struct SchematicFilter {
     pub page_num: Option<i64>,
     pub page_size: Option<i64>,
 }
+
+#[derive(Deserialize)]
+pub struct CommentFilter {
+    pub rating: Option<i32>,
+    pub author: Option<i32>,
+    pub date: Option<NaiveDateTime>,
+    pub sort_by: Option<String>,
+    pub sort_direction: Option<String>,
+    pub page_num: Option<i64>,
+    pub page_size: Option<i64>,
+}
