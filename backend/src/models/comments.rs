101073b7e33c5aa316a47c04dfb5ba1aa17e54c0
Rabbitminers <Rabbitminers2.0@gmail.com>
2023-04-17 21:46:53 +0100

    Added checks for comment ownership

--- backend/src/models/comments.rs
+++ backend/src/models/comments.rs
@@ -1,14 +1,18 @@
-use std::borrow::Borrow;
-
 use crate::{
     config::db::Connection,
-    models::schematic::Schematic,
-    models::user::User,
-    schema::comments::{self, dsl::*},   
+    models::{
+        schematic::Schematic,
+        filters::CommentFilter,
+        response::Page,
+        user::User
+    },
+    schema::comments::{self, dsl::*}, constants,
 };
 use chrono::{NaiveDateTime, Utc};
-use diesel::prelude::*;
+use diesel::{prelude::*, sqlite::Sqlite};
 use uuid::Uuid;
+
+use super::pagination::SortingAndPaging;
 
 
 #[derive(Identifiable, Associations, Serialize, Deserialize, Queryable, Insertable)]
@@ -26,21 +30,24 @@ pub struct Comment {
 #[derive(Insertable, AsChangeset)]
 #[table_name = "comments"]
 pub struct CommentDTO {
-    pub schematic_id: String,
     pub rating: i32,
     pub body: String,
 }
 
 
 impl Comment {
-    fn create(comment: CommentDTO, user_id: i32) -> Comment {
-        return Comment {
-            id: Self::generate_id(),
-            schematic_id: comment.schematic_id,
-            rating: comment.rating,
-            body: comment.body,
-            author: user_id,
-            date: Utc::now().naive_utc()
+    fn create(comment: CommentDTO, parent: String, user_id: i32) -> Result<Comment, String> {
+        if (1..=5).contains(&comment.rating) {
+            Ok(Comment {
+                id: Self::generate_id(),
+                schematic_id: parent,
+                rating: comment.rating,
+                body: comment.body,
+                author: user_id,
+                date: Utc::now().naive_utc()
+            })
+        } else {
+            Err(constants::MESSAGE_INVALID_RATING.to_string())
         }
     }
 
@@ -53,31 +60,86 @@ impl Comment {
             .execute(conn)
     }
 
-    pub fn post(comment: CommentDTO, user_id: i32, conn: &Connection) -> Result<String, String> {
+    pub fn filter(parent: String, filter: CommentFilter, conn: &Connection) -> QueryResult<Page<Comment>> {
+        let mut query = comments::table
+                .into_boxed::<Sqlite>()
+                .filter(schematic_id.eq(parent));
+
+        if let Some(i) = filter.rating {
+            query = query.filter(rating.eq(i));
+        }
+
+        if let Some(i) = filter.author {
+            query = query.filter(author.eq(i));
+        }
+
+        query
+            .paginate(
+            filter.page_num.unwrap_or(crate::constants::DEFAULT_PAGE_NUM),
+            )
+            .per_page(
+                filter.page_size.unwrap_or(crate::constants::DEFAULT_PER_PAGE),
+            )
+            .sort(
+        filter.sort_by.unwrap_or(crate::constants::EMPTY_STR.to_string()),
+ filter.sort_direction.unwrap_or(crate::constants::EMPTY_STR.to_string()),
+            )
+            .load_and_count_items::<Comment>(conn)
+    }
+
+    pub fn post(comment: CommentDTO, parent: String, user_id: i32, conn: &Connection) -> Result<String, String> {
         match comments.filter(author.eq(user_id)).first::<Comment>(conn) {
             Ok(_) => {
-                Err("You have already posted a comment on this schematic.".to_string())
+                Err(constants::MESSAGE_DUPLICATE_COMMENT.to_string())
             }
             Err(_) => {
-                let new_comment = Comment::create(comment, user_id);
-                let comment_id = new_comment.id.clone();
-                Comment::save_comment(new_comment, conn);
-                Ok(format!("Posted new comment with id: {}", comment_id))
+                match Self::create(comment,  parent, user_id) {
+                    Ok(new_comment) => {
+                        let comment_id = new_comment.id.clone();
+                        Comment::save_comment(new_comment, conn);
+                        Ok(format!("Posted new comment with id: {}", comment_id))
+                    }
+                    Err(e) => Err(e)
+                }
             }
         }
     }
 
-    pub fn delete(comment_id: String, conn: &Connection) -> QueryResult<usize> {
-        diesel::delete(comments.filter(id.eq(comment_id))).execute(conn)
+    pub fn get_by_id(comment_id: &String, conn: &Connection) -> Option<Comment> {
+        comments.filter(id.eq(comment_id))
+            .first(conn)
+            .optional()
+            .unwrap_or(None)
     }
 
-    pub fn update(comment_id: String, updated_comment: CommentDTO,  conn: &Connection) -> QueryResult<usize> {
-        diesel::update(comments.find(comment_id))
-            .set(&updated_comment)
-            .execute(conn)
+    pub fn delete(comment_id: String, user: User, conn: &Connection) -> Result<QueryResult<usize>, String> {
+        match Self::get_by_id(&comment_id, conn) {
+            Some(c) if c.author == user.id => {
+                Ok(diesel::delete(comments.find(comment_id)).execute(conn))
+            }
+            Some(_) => Err(constants::MESSAGE_CAN_NOT_DELETE_COMMENT.to_string()),
+            None => Err(constants::MESSAGE_COMMENT_NOT_FOUND.to_string()),
+        }
+    }
+
+    pub fn update(comment_id: String, user: User, updated_comment: CommentDTO,  conn: &Connection) -> Result<QueryResult<usize>, String> {
+        match Self::get_by_id(&comment_id, conn) {
+            Some(c) if c.author == user.id => {
+                Ok(diesel::update(comments.find(comment_id))
+                    .set(&updated_comment)
+                    .execute(conn)
+                )
+            }
+            Some(_) => Err(constants::MESSAGE_CAN_NOT_MODIFY_COMMENT.to_string()),
+            None => Err(constants::MESSAGE_COMMENT_NOT_FOUND.to_string()),
+        }
     }
 
     pub fn generate_id() -> String {
         Uuid::new_v4().to_simple().to_string()
+    }
+
+    pub fn is_owned_by(&self, user: &User) -> bool {
+        self.author == user.id
     }
 }
